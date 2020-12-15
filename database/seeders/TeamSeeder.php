43c22e772e9556a8fbbe7fc24691558d7a9b8390
Vitalii <vitalik6230@gmail.com>
2020-12-15 15:39:29 +0200

    Simulation add seeder

---
+++ database/seeders/TeamSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Team;
+use Illuminate\Database\Seeder;
+
+class TeamSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 1; $i <= 20; $i++) {
+            Team::create([
+                'name' => 'Team ' . $i,
+            ]);
+        }
+    }
+}
